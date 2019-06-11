39760536abc06319b9b23e2c71f65a45087b01d4
zhdaurenkyzy <51210391+zhdaurenkyzy@users.noreply.github.com>
2019-06-11 16:01:26 +0600

    Update TaskFirstA.java

--- TaskFirstA.java
+++ TaskFirstA.java
@@ -3,16 +3,17 @@ package com.company;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class aTaskOne {
+public class TaskFirstA {
     public static void main(String[] args) {
 
         String str = "Meet my family. There are five of us – my parents, my elder brother, my baby sister and me. First, meet my mum and dad, Jane and Michael.";
         int k = 4;
         String[] words = str.split("[,.\\s]|\\s");
         for(String word: words){
-         //System.out.println(word);
             if(k<word.length()){
-                word = word.replace(word.charAt(k-1), '*');
-        }
+            word = word.replace(word.charAt(k-1), '*');
+            }
         System.out.println(word);
+        }
+    }
 }
