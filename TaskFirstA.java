c2f26b649f15830da039a344c17d881fba65843a
zhdaurenkyzy <51210391+zhdaurenkyzy@users.noreply.github.com>
2019-06-15 01:04:57 +0600

    Update TaskFirstA.java

--- TaskFirstA.java
+++ TaskFirstA.java
@@ -6,7 +6,8 @@ import java.util.regex.Pattern;
 public class TaskFirstA {
     public static void main(String[] args) {
 
-        String str = "Meet my family. There are five of us – my parents, my elder brother, my baby sister and me. First, meet my mum and dad, Jane and Michael.";
+        String str = "Meet my family. There are five of us – my parents, my elder brother,
+            + my baby sister and me. First, meet my mum and dad, Jane and Michael.";
         int k = 4;
         String[] words = str.split("[,.\\s]|\\s");
         for(String word: words){
