efa490dce5d6fc5db05a3391213147385123ed32
zhdaurenkyzy <51210391+zhdaurenkyzy@users.noreply.github.com>
2019-06-15 01:05:36 +0600

    Update TaskSecondA.java

--- TaskSecondA.java
+++ TaskSecondA.java
@@ -4,7 +4,8 @@ public class TaskSecondA {
     public static void main(String[] args) {
        StringBuilder stringBuilder = new StringBuilder("Hello world");
        StringBuilder stringBuilderSecond2 = new StringBuilder(stringBuilder);
-       String[] list = {"a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+       String[] list = {"a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", 
+                        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
        for (int i = 0; i < stringBuilder.length(); i++) {
 
            for(int j=0; j< list.length; j++){
